ede33bbd3ea792da8ac5774a79eedf442c829d8b
U-BSD\398121 <398121@SHS-DGDSCS2.beaverton.k12.or.us>
2022-10-03 14:02:07 -0700

    Final Project Done

--- ArmaanTTT.cpp
+++ ArmaanTTT.cpp
@@ -1,16 +1,25 @@
+/*
+ * Author: Armaan Hajar
+ * Description: A simple TicTacToe game in the console
+ * Date: 10/3/2022
+ */
+
+
 #include <iostream>
 #include <string>
 #include <cstring>
 
 using namespace std;
 
+// calling all the functions
 void printBoard(char board[3][3]);
 void clearBoard(char board[3][3]);
 bool checkWin(char board[3][3], int player);
 bool checkTie(char board[3][3], int player);
-void restartMessage(char board[3][3], bool newgame);
+void restartMessage(char board[3][3], bool& newgame);
 
 int main() {
+  // initializing variables
   char board[3][3];
   char str[3];
 
@@ -25,21 +34,12 @@ int main() {
 
   clearBoard(board);
 
+  // main game code, when user is done playing, newgame becomes false
   while (newgame == true) {
 
-    if (checkWin(board, X_MOVE) == true) {
-      cout << "win x" << endl;
-    }
-    if (checkWin(board, O_MOVE) == true) {
-      cout << "win o" << endl;
-    }
-    if (checkTie(board, X_MOVE) == true) {
-      cout << "tie x" << endl;
-    }
-    if (checkTie(board, O_MOVE) == true) {
-      cout << "tie o" << endl;
-    }
+    turn = X_TURN;
 
+    // when a win or tie hasnt been detected, run the game code
     while (checkWin(board, X_MOVE) == false && checkWin(board, O_MOVE) == false &&
 	   checkTie(board, X_MOVE) == false && checkTie(board, O_MOVE) == false) {
       if (turn == X_TURN) {
@@ -55,59 +55,69 @@ int main() {
       cin.get();
 
       cout << "-----------------" << endl;
-      
+
+      // input validation
       if (strlen(str) != 2) {
-	cout << "Please Enter A Valid Position 1!" << endl;
+	cout << "Please Enter A Valid Position!" << endl;
       }
       else if (str[0] != 'a' && str[0] != 'b' && str[0] != 'c') {
-	cout << "Please Enter A Valid Position 2!" << endl;
+	cout << "Please Enter A Valid Position!" << endl;
       }
       else if (str[1] != '1' && str[1] != '2' && str[1] != '3') {
-	cout << "Please Enter A Valid Position 3!" << endl;
+	cout << "Please Enter A Valid Position!" << endl;
       }
+      // if input is valid, add it to the board
       else {
 	int column = str[0] - 'a';
 	int row = str[1] - '1';
 	if (board[row][column] == '_') {
-	  if (turn == X_TURN) {
+	  if (turn == X_TURN) { // x move
 	    board[row][column] = X_MOVE;
 	    turn = O_TURN;
 	  }
-	  else {
+	  else { // o move
 	    board[row][column] = O_MOVE;
 	    turn = X_TURN;
 	  }
 	}
-	else {
+	else { // when the board position is already taken
 	  cout << "You Cannot Select This Location!" << endl;
 	}
       }
     }
+    // when x win is detected
     if (checkWin(board, X_MOVE) == true) {
       X_WINS++;
       printBoard(board);
+      cout << "-----------------" << endl;
       cout << "X Wins!" << endl;
       cout << "X: " << X_WINS << endl;
       cout << "O: " << O_WINS << endl;
       restartMessage(board, newgame);
     }
+    // when o win is detected
     else if (checkWin(board, O_MOVE) == true) {
       O_WINS++;
       printBoard(board);
+      cout << "-----------------" << endl;
       cout << "O Wins!" << endl;
       cout << "X: " << X_WINS << endl;
       cout << "O: " << O_WINS << endl;
       restartMessage(board, newgame);
     }
+    // when tie is detected
     else if (checkTie(board, O_MOVE) == true) {
       printBoard(board);
+      cout << "-----------------" << endl;
       cout << "It's A Tie" << endl;
       cout << "X: " << X_WINS << endl;
       cout << "O: " << O_WINS << endl;
       restartMessage(board, newgame);
     }
+    // when tie is detected 
     else if (checkTie(board, X_MOVE) == true) {
       printBoard(board);
+      cout << "-----------------" << endl;
       cout << "It's A Tie" << endl;
       cout << "X: " << X_WINS << endl;
       cout << "O: " << O_WINS << endl;
@@ -118,6 +128,7 @@ int main() {
   return 0;
 }
 
+// prints board
 void printBoard(char board[3][3]) {
   cout << "  1 2 3" << endl;
   cout << "a " << board[0][0] << ' ' << board[1][0] << ' ' << board[2][0] << endl;
@@ -125,6 +136,7 @@ void printBoard(char board[3][3]) {
   cout << "c " << board[0][2] << ' ' << board[1][2] << ' ' << board[2][2] << endl;
 }
 
+// sets every board position to an underscore
 void clearBoard(char board[3][3]) {
   for (int a = 0; a < 3; a++) {
     for (int b = 0; b < 3; b++) {
@@ -133,6 +145,7 @@ void clearBoard(char board[3][3]) {
   }
 }
 
+// win detection
 bool checkWin(char board[3][3], int player) {
   if (board[0][0] == player && board[0][1] == player && board[0][2] == player) {
     return true;
@@ -161,6 +174,7 @@ bool checkWin(char board[3][3], int player) {
   return false;
 }
 
+// if there are no empty spaces on the board and no win has been detected, a tie has happened
 bool checkTie(char board[3][3], int player) {
   for (int a = 0; a < 3; a++) {
     for (int b = 0; b < 3; b++) {
@@ -172,22 +186,25 @@ bool checkTie(char board[3][3], int player) {
   return true;
 }
 
-void restartMessage(char board[3][3], bool newgame) {
+// after every win or tie, this function is called to see if the user wants to play again
+void restartMessage(char board[3][3], bool& newgame) {
   char yorn;
-  
+
+  cout << "-----------------" << endl;
   cout << "Do You Want To Play Again? (Y/N)" << endl;
   cin.get(yorn);
+  cin.get();
 
   if (yorn == 'Y' || yorn == 'y') {
     cout << "Restarting!" << endl;
     clearBoard(board);
+    cout << endl;
   }
   else {
     cout << "Thank You For Playing" << endl;
     newgame = false;
   }
-  return newgame;
 }
 
 // hello jeff, you are the best
-// test again
+// you too rahul
