b6600451654448419a66cf4ab042ddbd1af2b400
ArmaanHajar <armaanhajar@icloud.com>
2022-10-04 13:28:41 -0700

    small fix

--- ArmaanTTT.cpp
+++ ArmaanTTT.cpp
@@ -159,7 +159,7 @@ bool checkWin(char board[3][3], int player) {
   if (board[0][0] == player && board[1][0] == player && board[2][0] == player) {
     return true;
   }
-  if (board[0][1] == player && board[1][1] == player && board[2][0] == player) {
+  if (board[0][1] == player && board[1][1] == player && board[2][1] == player) {
     return true;
   }
   if (board[0][2] == player && board[1][2] == player && board[2][2] == player) {
